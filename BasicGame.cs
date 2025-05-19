45b44e61f91a59e96dbf78af0edeaaa0d0a3bcfe
Daniel Park <intheimageofyeshua@gmail.com>
2025-05-19 09:21:30 +0900

    Mouse State Update

--- BasicGame.cs
+++ BasicGame.cs
@@ -10,7 +10,8 @@ public class BasicGame : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-    Texture2D balloon;
+    Texture2D balloon, background;
+    Vector2 balloonPosition;
 
     public BasicGame()
     {
@@ -32,16 +33,16 @@ public class BasicGame : Game
 
         // TODO: use this.Content to load your game content here
         balloon = Content.Load<Texture2D>("spr_lives");
-
+        background = Content.Load<Texture2D>("spr_background");
         MediaPlayer.Play(Content.Load<Song>("snd_music"));
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+        MouseState currentMouseState = Mouse.GetState();
 
         // TODO: Add your update logic here
+        balloonPosition = new Vector2(currentMouseState.X, currentMouseState.Y);
 
         base.Update(gameTime);
     }
@@ -52,7 +53,8 @@ public class BasicGame : Game
 
         // TODO: Add your drawing code here
         _spriteBatch.Begin();
-        _spriteBatch.Draw(balloon, Vector2.Zero, Color.White);
+        _spriteBatch.Draw(background, Vector2.Zero, Color.White);
+        _spriteBatch.Draw(balloon, balloonPosition, Color.White);
         _spriteBatch.End();
 
         base.Draw(gameTime);
